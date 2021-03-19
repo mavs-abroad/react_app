96a11caca0b3bab9598767855a47cdcd4c1c767c
Sandesh Koirala <sandesh.koirala@mavs.uta.edu>
2021-03-19 00:04:57 -0500

    Edited Page2 title

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -91,7 +91,7 @@ function TabTwoNavigator() {
       <TabTwoStack.Screen
         name="TabTwoScreen"
         component={TabTwoScreen}
-        options={{ headerTitle: 'Tab Two Title' }}
+        options={{ headerTitle: 'Sites' }}
       />
     </TabTwoStack.Navigator>
   );
