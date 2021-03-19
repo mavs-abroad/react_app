ecf8931ee002022e2f53bedb99932fd8f3595e6f
Sandesh Koirala <sandesh.koirala@mavs.uta.edu>
2021-03-19 12:37:36 -0500

    updated tabone header

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -77,7 +77,7 @@ function TabOneNavigator() {
       <TabOneStack.Screen
         name="TabOneScreen"
         component={TabOneScreen}
-        options={{ headerTitle: 'Tab One Title' }}
+        options={{ headerTitle: 'Welcome  |  ようこそ' }}
       />
     </TabOneStack.Navigator>
   );
