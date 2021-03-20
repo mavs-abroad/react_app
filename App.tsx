941541c1642e0aaa39504f59a913b3b9457fc44a
Sandesh Koirala <sandesh.koirala@mavs.uta.edu>
2021-03-20 17:08:36 -0500

    connected to firebase sdk

--- App.tsx
+++ App.tsx
@@ -6,6 +6,21 @@ import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
+import * as firebase from "firebase";
+var firebaseConfig = {
+  apiKey: "AIzaSyBIkzUAEQTEF6h4K8Ui_Z8mF6WeM1f1Ff4",
+  authDomain: "mavs-abroad-dda3b.firebaseapp.com",
+  databaseURL: "https://mavs-abroad-dda3b-default-rtdb.firebaseio.com",
+  projectId: "mavs-abroad-dda3b",
+  storageBucket: "mavs-abroad-dda3b.appspot.com",
+  messagingSenderId: "583288162608",
+  appId: "1:583288162608:web:e6cba5ee73bad917d7a0f5",
+  measurementId: "G-F8WJE0L2W1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+//firebase.analytics();
+
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
