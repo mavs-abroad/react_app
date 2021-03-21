12c4263bddcb2792a3265475db63cb39e0b8197b
Sandesh Koirala <sandesh.koirala@mavs.uta.edu>
2021-03-20 19:00:50 -0500

    screen 1 cleanup for demo

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -7,9 +7,12 @@ import { Text, View } from '../components/Themed';
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.weather}> Current Weather at "Location"</Text>
+      <View style={styles.weather_container}></View>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Text style={styles.notif}> Notifications</Text>
+      <View style={styles.notif_container}></View>
+      {/* <EditScreenInfo path="/screens/TabOneScreen.tsx" /> */}
     </View>
   );
 }
@@ -20,13 +23,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  title: {
+  weather: {
     fontSize: 20,
     fontWeight: 'bold',
+    marginVertical:20,
+  },
+  notif: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginVertical:20,
   },
   separator: {
-    marginVertical: 30,
+    marginVertical: 5,
     height: 1,
     width: '80%',
+  },
+  weather_container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  notif_container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
