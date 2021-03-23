9c3af2ec7a2e05c8311de312f3267f05fc63b773
Sandesh Koirala <sandesh.koirala@mavs.uta.edu>
2021-03-22 20:06:05 -0500

    removed notifications view, will add a notif icon to header

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,18 +1,14 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet} from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.weather}> Current Weather at "Location"</Text>
-      <View style={styles.weather_container}></View>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Text style={styles.notif}> Notifications</Text>
-      <View style={styles.notif_container}></View>
-      {/* <EditScreenInfo path="/screens/TabOneScreen.tsx" /> */}
+      <View style={styles.weather_container}>
+        <Text style = {styles.weather_text}>Sunny</Text>
+      </View>
     </View>
   );
 }
@@ -23,28 +19,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  weather: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginVertical:20,
-  },
-  notif: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginVertical:20,
-  },
-  separator: {
-    marginVertical: 5,
-    height: 1,
-    width: '80%',
-  },
   weather_container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  notif_container: {
-    flex: 1,
+    flex: 6,
+    // backgroundColor: "aqua",
+    width:Dimensions.get('window').width,
     alignItems: 'center',
     justifyContent: 'center',
   },
