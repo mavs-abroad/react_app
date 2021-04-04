997c4718e0d4d5dc1f9fe8c97bce42547c10d961
Sandesh Koirala <sandesh.koirala@mavs.uta.edu>
2021-04-03 19:17:24 -0500

    hardcoded translations on tab 4

--- screens/TabFourScreen.tsx
+++ screens/TabFourScreen.tsx
@@ -1,58 +1,82 @@
 import * as React from 'react';
-import { StyleSheet,ScrollView } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { StyleSheet, SafeAreaView, SectionList } from 'react-native';
+import { Text, View} from '../components/Themed';
+import Constants from 'expo-constants';
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      {/* // <ScrollView style={styles.scrollView}>
-      // <Text style={styles.title}>Good Morning</Text>
-      // <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      // <Text style={styles.title}>Good Morning</Text>
-      // <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      // <Text style={styles.title}>Good Morning</Text>
-      // <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      // <Text style={styles.title}>Good Morning</Text>
-      // <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      // <Text style={styles.title}>Good Morning</Text>
-      // <Text style={styles.title}>Good Morning</Text>
-      // <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      // <Text style={styles.title}>Good Morning</Text>
-      // <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      // <Text style={styles.title}>Good Morning</Text>
-      // <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      // <Text style={styles.title}>Good Morning</Text>
-      // <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      // <Text style={styles.title}>Good Morning</Text>
-      // <Text style={styles.title}>Good Morning</Text>
-      // <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      // <Text style={styles.title}>Good Morning</Text>
-      // <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      // <Text style={styles.title}>Good Morning</Text>
-      // <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      // <Text style={styles.title}>Good Morning</Text>
-      // <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      // <Text style={styles.title}>Good Morning</Text>
-      // </ScrollView> */}
-    </View>
+    <SafeAreaView style={styles.container}>
+      <SectionList
+        sections={DATA}
+        keyExtractor={(item, index) => item + index}
+        renderItem={({ item }) => <Item title={item} />}
+        renderSectionHeader={({ section: { title } }) => <Text style={styles.header}>{title}</Text>}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    //alignItems: 'center',
     justifyContent: 'center',
+    padding:20,
   },
   title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+    fontSize: 18,
+    color:"dodgerblue",
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  item: {
+    backgroundColor: 'aliceblue',
+    padding:10,
+    marginLeft:20,
+  },
+  header: {
+    fontSize: 20,
+    fontWeight:"bold",
+    backgroundColor: 'silver',
+    color:"darkblue",
+    padding:8,
   },
 });
+
+const DATA = [
+  {
+    title: 'Yes',
+    data: [ 'hai','はい'],
+  },
+  {
+    title: 'No',
+    data: ['lie','いいえ'],
+  },
+  {
+    title: 'Please',
+    data: ['O-negai shimasu', 'おねがいします'],
+  },
+  {
+    title: 'Thank you',
+    data: ['Arigatō', 'ありがとう'],
+  },
+  {
+    title: "You're welcome",
+    data: [ 'Dōitashimashite','どういたしまして'],
+  },
+  {
+    title: 'Excuse me',
+    data: ['Sumimasen','すみません'],
+  },
+  {
+    title: 'I am sorry',
+    data: ['Gomennasai', 'ごめんなさい'],
+  },
+  {
+    title: 'Do you speak English?',
+    data: ['Eigo o hanasemasu ka', 'えいごをはなせますか'],
+  },
+];
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
